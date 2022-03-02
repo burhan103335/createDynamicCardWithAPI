41b00b46b64e641a2463002f7e70435cfbb4ef49
Burhan <burhan01733700745@gmail.com>
2022-03-02 02:54:08 +0000

    manually added a array for title and body

---
+++ script.js
@@ -0,0 +1,38 @@
+const posts = [
+
+    {
+        title : "This is title1",
+        body : "this is body1",
+    },
+    {
+        title : "This is title2",
+        body : "this is body2",
+    },
+    {
+        title : "This is title3",
+        body : "this is body3",
+    },
+    {
+        title : "This is title4",
+        body : "this is body4",
+    },
+    {
+        title : "This is title5",
+        body : "this is body5",
+    },
+    {
+        title : "This is title6",
+        body : "this is body6",
+    },
+    {
+        title : "This is title7",
+        body : "this is body7",
+    },
+    {
+        title : "This is title8",
+        body : "this is body8",
+    },
+];
+
+//selection
+
